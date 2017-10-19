f65ba4f648fcb0e8babbf2edfa849e2a2d5d119e
nick121212 <nick121212@126.com>
2017-10-19 09:26:46 +0800

    change

--- packages/fx-schema-form-antd/src/factory.tsx
+++ packages/fx-schema-form-antd/src/factory.tsx
@@ -27,22 +27,22 @@ for (let key in fields) {
     }
 }
 
-for (let key in widgets) {
-    if (widgets.hasOwnProperty(key)) {
-        let widget = widgets[key];
+// for (let key in widgets) {
+//     if (widgets.hasOwnProperty(key)) {
+//         let widget = widgets[key];
 
-        defaultTheme.widgetFactory.add(key, widget);
-    }
-}
+//         defaultTheme.widgetFactory.add(key, widget);
+//     }
+// }
 
-for (let key in templates) {
-    if (templates.hasOwnProperty(key)) {
-        let template = templates[key];
+// for (let key in templates) {
+//     if (templates.hasOwnProperty(key)) {
+//         let template = templates[key];
 
-        defaultTheme.tempFactory.add(key, template);
-        defaultTheme.tempFactory.lock(key);
-    }
-}
+//         defaultTheme.tempFactory.add(key, template);
+//         defaultTheme.tempFactory.lock(key);
+//     }
+// }
 
 export {
     nsFactory,
